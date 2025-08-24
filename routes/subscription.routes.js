8e9f6913723c6c719ee43e5358eb9d94e4fa0079
Chun Sokna <chunsokna111@gmail.com>
2025-08-24 13:23:10 +0700

    Add subscription detail, create, update, delete, cancel, and upcoming renewal routes to the subscription API

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -8,4 +8,46 @@ subscriptionRouter.get("/v1/subscriptions", (req, res) => {
 	});
 });
 
+subscriptionRouter.get("/v1/subscriptions/:id", (req, res) => {
+	res.json({
+		message: `This is a subscription detail API for ID: ${req.params.id}`,
+	});
+});
+
+subscriptionRouter.post("/v1/subscriptions/create", (req, res) => {
+	res.json({
+		message: "This is a create subscription API",
+	});
+});
+
+subscriptionRouter.get("/v1/subscriptions/user/:id", (req, res) => {
+	res.json({
+		message: `This is a get subscriptions by user ID API for User ID: ${req.params.userId}`,
+	});
+});
+
+subscriptionRouter.put("/v1/subscriptions/:id/update", (req, res) => {
+	res.json({
+		message: `This is an update subscription API for ID: ${req.params.id}`,
+	});
+});
+
+subscriptionRouter.delete("/v1/subscriptions/:id/delete", (req, res) => {
+	res.json({
+		message: `This is a delete subscription API for ID: ${req.params.id}`,
+	});
+});
+
+subscriptionRouter.post("/v1/subscriptions/:id/cancel", (req, res) => {
+	res.json({
+		message: `This is a cancel subscription API for ID: ${req.params.id}`,
+	});
+});
+
+subscriptionRouter.post("/v1/subscriptions/upcoming_renewal", (req, res) => {
+	res.json({
+		message: "This is an upcoming_renewal subscription API",
+	});
+});
+
 export default subscriptionRouter;
