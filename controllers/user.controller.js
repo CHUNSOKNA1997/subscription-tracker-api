5ee101a29a2e241b6df02a1f756c065282124452
Chun Sokna <chunsokna111@gmail.com>
2025-08-27 11:05:35 +0700

    Add createUser function to user controller for user registration; update user routes to implement new endpoint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 import prisma from "../prisma/prisma.js";
 import userCollection from "../resources/userCollection.js";
 import userResource from "../resources/userResource.js";
+import bcrypt from "bcryptjs";
 
 export const getUsers = async (req, res) => {
 	try {
@@ -46,4 +47,58 @@ export const getUser = async (req, res) => {
 			error: error.message,
 		});
 	}
+};
+
+export const createUser = async (req, res) => {
+	await prisma.$transaction(async (prisma) => {
+		try {
+			const { name, email, password, password_confirmation } = req.body;
+
+			const existingUser = await prisma.user.findUnique({
+				where: { email },
+			});
+
+			if (existingUser) {
+				return res.error({
+					message: "A user with this email already exists",
+				});
+			}
+
+			console.log(password, password_confirmation);
+
+			if (password !== password_confirmation) {
+				return res.error({
+					message: "Password and password confirmation do not match",
+				});
+			}
+
+			const salt = await bcrypt.genSalt(10);
+
+			const hashPassword = await bcrypt.hash(password, salt);
+
+			const newUser = await prisma.user.create({
+				data: {
+					name: name,
+					email: email,
+					password: hashPassword,
+				},
+			});
+
+			if (newUser) {
+				res.success({
+					message: "User created successfully",
+					user: newUser,
+				});
+			} else {
+				res.error({
+					message: "Failed to create user",
+				});
+			}
+		} catch (error) {
+			res.error({
+				message: "An error occurred while creating the user",
+				error: error.message,
+			});
+		}
+	});
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getUser, getUsers } from "../controllers/user.controller.js";
+import {
+	getUser,
+	getUsers,
+	createUser,
+} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
@@ -7,12 +11,7 @@ userRouter.get("/v1/users", getUsers);
 
 userRouter.get("/v1/users/:uuid", getUser);
 
-userRouter.post("/v1/users/create", (req, res) => {
-	res.status(400).json({
-		message:
-			"This endpoint is not implemented. Please use the /api/v1/auth/signup endpoint to create a user.",
-	});
-});
+userRouter.post("/v1/users/create", createUser);
 
 userRouter.put("/v1/users/:id", (req, res) => {
 	res.json({
